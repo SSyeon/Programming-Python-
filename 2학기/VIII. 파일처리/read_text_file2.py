2fdb445d6f75bec8cbe0432e1b8607483ee42c9c
ssyeon <tjdusdlfkrhd@naver.com>
2019-10-08 11:07:36 +0900

    write text file, read text(ama), readline, 가격의 총합

--- 2학기/VIII. 파일처리/read_text_file2.py
+++ 2학기/VIII. 파일처리/read_text_file2.py
@@ -1,12 +1,16 @@
 fi = open("history.ama", "r", encoding="utf8")
-
+sum=0
 while True:
     data = fi.readline()
     if not data:
         break
-    print(data ,end ="")
+    # print(data ,end ="")
     # 한줄안에서 메뉴,이름, 가격 등등 자르기......
+    l=data.split() # 화이트 스페이스 : /t,/n 띄어쓰기 있는걸 다자름
+    sum += int(l[1]) #음료의 가격 합계를 구한다.
+
+print("총 금액" + str(sum) + "원")
+    # print(l[1])
     #총금액 계산 후 출력하기
 
 fi.close()
-
