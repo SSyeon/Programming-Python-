91902f905b2d90b16866a1fae8665aa6643a4ef6
ssyeon <tjdusdlfkrhd@naver.com>
2019-11-29 13:51:22 +0900

    Daum Movie Boxoffice crawling

--- 2학기/IX. 프로젝트/movie_boxoffice.py
+++ 2학기/IX. 프로젝트/movie_boxoffice.py
@@ -4,7 +4,13 @@ from bs4 import BeautifulSoup
 
 
 if __name__ == '__main__':
-    with urlopen("https://movie.daum.net/boxoffice/weekly") as response:
+    with urlopen("https://movie.daum.net/boxoffice/yearly") as response:
         soup  =  BeautifulSoup(response, "lxml")
 
-        print(soup)
+        # print(soup)
+    movie_titles = soup.find_all("a", attrs={"class":"link_g"})
+    print("<<<다음 영화 박스오피스>>>")
+    n =  1
+    for movie_title in movie_titles[2:] :
+        print(n,"위",movie_title.text)
+        n+=1
